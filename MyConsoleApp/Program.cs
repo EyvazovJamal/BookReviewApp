a46d6fdd13445ae0b9330f0b2ea52c9b775a4bb1
EyvazovJamal <eyvazovcamal2006@gmail.com>
2025-02-22 17:06:30 +0400

    Message

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
